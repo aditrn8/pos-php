8160515a2d3bd77ee0d6898a9b726247c4562d49
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-07-07 19:09:00 +0700

    print cetak

--- application/views/kasir/transaksi/cetak.php
+++ application/views/kasir/transaksi/cetak.php
@@ -1,6 +1,11 @@
 <title>Laporan Transaksi (<?= $q1->Nomor_Invoice ?>)</title>
 <h1 align="center">Detail Laporan Transaksi (<?= $q1->Nomor_Invoice ?>)</h1>
 
+<h3 align="center">
+    Toko Satria Nugget
+    Jl Klambir V Ruko Janetti No 4, Medan
+</h3>
+
 <script>
     window.print();
 </script>
